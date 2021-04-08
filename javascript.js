208093b828e448c32914a097bf1c8cdfc40f26f1
Matt Benson <rabenson6@msn.com>
2021-04-08 16:15:29 -0700

    challenge uploadwith revisions

--- javascript.js
+++ javascript.js
@@ -1,15 +1,13 @@
+// Declaring variables, two display containers, random color button, and search text box
 const swatchContainer = document.getElementById("swatchContainer");
 const lowerContainer = document.getElementById("lowerContainer");
 const randomBtn = document.getElementById("randomBtn");
 const searchText = document.getElementById("searchText");
-const selectedIndex = document.getElementsByClassName("indexList");
-
-
 // Array of named colors, contains most named colors in HTML
 colorList=['#FFC0CB','#FFB6C1','#FF69B4','#FF1493','#DB7093','#C71585','#E6E6FA','#D8BFD8','#DDA0DD','#DA70D6','#EE82EE','#FF00FF','#FF00FF','#BA55D3','#9932CC','#9400D3','#8A2BE2','#8B008B','#800080','#9370DB','#7B68EE','#6A5ACD','#483D8B','#663399','#4B0082','#FFA07A','#FA8072','#E9967A','#F08080','#CD5C5C','#DC143C','#FF0000','#B22222','#8B0000','#FFA500','#FF8C00','#FF7F50','#FF6347','#FF4500','#FFD700','#FFFF00','#FFFFE0','#FFFACD','#FAFAD2','#FFEFD5','#FFE4B5','#FFDAB9','#EEE8AA','#F0E68C','#BDB76B','#ADFF2F','#7FFF00','#7CFC00','#00FF00','#32CD32','#98FB98','#90EE90','#00FA9A','#00FF7F','#3CB371','#2E8B57','#228B22','#008000','#006400','#9ACD32','#6B8E23','#556B2F','#66CDAA','#8FBC8F','#20B2AA','#008B8B','#008080','#00FFFF','#00FFFF','#E0FFFF','#AFEEEE','#7FFFD4','#40E0D0','#48D1CC','#00CED1','#5F9EA0','#4682B4','#B0C4DE','#ADD8E6','#B0E0E6','#87CEFA','#87CEEB','#6495ED','#00BFFF','#1E90FF','#4169E1','#0000FF','#0000CD','#00008B','#000080','#191970','#FFF8DC','#FFEBCD','#FFE4C4','#FFDEAD','#F5DEB3','#DEB887','#D2B48C','#BC8F8F','#F4A460','#DAA520','#B8860B','#CD853F','#D2691E','#808000','#8B4513','#A0522D','#A52A2A','#800000','#FFFAFA','#F0FFF0','#F5FFFA','#F0FFFF','#F0F8FF','#F8F8FF','#F5F5F5','#FFF5EE','#F5F5DC','#FDF5E6','#FFFAF0','#FFFFF0','#FAEBD7','#FAF0E6','#FFF0F5','#FFE4E1','#DCDCDC']
-// Loads initial swatch page on load
+// Loads initial swatch page on page load
 $(document).ready(generateswatchHtml());
-// Generates initial swatch container display, checks for page index in displaying colors
+// Generates initial swatch container display, checks for page index to display correct colors
 function generateswatchHtml(pageIndex){
     let i = 0;
     if(pageIndex > 0){
@@ -156,7 +154,7 @@ $("#searchText").keyup(function(event) {
         isValidColor(searchText.value);
     }
 });
-// Checks to see if the entered text is a valid color, if not the user gets an error message
+// Checks to see if the entered text in the search form is a valid color, if not the user gets an error message
 function isValidColor(strColor) {
     var s = new Option().style;
     s.color = strColor;
@@ -179,19 +177,19 @@ function getHexColor(colorStr) {
 function generateShades(rgb){
     $("#lowerContainer").append(`
         <div class="col-md-2">
-                <div class="colorSwatch" id="`+rgb+`" onclick="detailView(this.id)">
-                    <div class="shadeBox" style="background-color:`+rgb+`">
-                    </div>
-                    <div class="colorName">`+rgb+`
-                    </div>
+            <div class="colorSwatch" id="`+rgb+`" onclick="detailView(this.id)">
+                <div class="shadeBox" style="background-color:`+rgb+`">
                 </div>
-            </div>`
+                <div class="colorName">`+rgb+`
+                </div>
+            </div>
+        </div>`
     )
 }
-// Generates the clear button in the detail view
+// Generates padding div to the end of the swatch row, and the clear button in the detail view,
 function generateClearbtn(){
     $("#lowerContainer").append(
-    `   <div class = "col-md-1">
+        `<div class = "col-md-1">
         </div>
     </div>
     <div class = "row buttonRow">
@@ -211,7 +209,7 @@ function generatePaddingCol(){
         </div>`
     )
 }
-//  Function used to gather shades for Detail view, uses luminosity variations from main color to generate shades. Also creates a shades array to sort out duplicate shades
+//  Function used to gather shades for Detail view, uses luminosity variations from main color to generate shades. Also creates a shades array to filter out duplicate shades
 function getShades(mainColor){
     $("#lowerContainer").empty();
     let lums = [-.2, -.1, 0, .1, .2]
@@ -271,7 +269,7 @@ function detailView(mainColor) {
         )
         getShades(mainColor);
 }
-// Handles Random Color Button Click, selecting a random color from the colorList array to display
+// Handles Random Color Button Click, selecting a random color from the colorList array to send to the detailView function
 $("#randomBtn").on("click", function(event) {
     event.preventDefault();
     let mainColor = colorList[Math.floor(Math.random() * colorList.length)]
